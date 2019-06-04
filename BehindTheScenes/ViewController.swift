6f4eb52b711397242a2fc708b83d74b4c9985cb8
Simeon <simeon@twolivesleft.com>
2019-06-04 19:37:56 +0930

    Initial Commit

---
+++ BehindTheScenes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BehindTheScenes
+//
+//  Created by Simeon Saint-Saens on 4/6/19.
+//  Copyright © 2019 Two Lives Left. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
